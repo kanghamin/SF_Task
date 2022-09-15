e690fc79548f8163fe9e1c74b80270cf660b1295
hamink <gogokhm1@gmail.com>
2022-09-14 21:29:32 -0600

    fetch Coingecko Api

--- exchange_directory/src/App.js
+++ exchange_directory/src/App.js
@@ -1,6 +1,29 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [page, setPage] = useState(1);
+  const [exchanges, setExchanges] = useState([]);
+
+  // const loadMorePage = () => {
+  //   setPage(page + 1);
+  // }
+
+  useEffect(() => {
+    fetch(
+      `https://api.coingecko.com/api/v3/exchanges?per_page=10&page=${page}`,
+      {
+        method: "GET",
+        headers: new Headers({
+          Accept: "application/json"
+        })
+      }
+    )
+    .then(res => res.json())
+    .then(response => {
+      setExchanges(response)
+    })
+  }, [page]);
  
   return (
     <div className="App">
